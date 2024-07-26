06da9e4c5b26c5e6aad56ac21af37ecc7f1616ae
Аngie Bu <75640543+88v88@users.noreply.github.com>
2024-07-26 17:08:06 -0400

    Condense duplicate CHF3 equations

--- sentaurus/plasma_model.cmd
+++ sentaurus/plasma_model.cmd
@@ -1,8 +1,8 @@
-# Plasma model for CF4/Ar/CHF3 ICP/RIE etch
+# Plasma model for CF4/CHF3/Ar ICP/RIE etch
 # References:
 # CF4 Reactions from Tonelli et. al. 
 # "A global model study of low pressure high density CF4 discharge"
-#  CHF3 Reactions & all electron collision energy losses from Ho et. al. 
+#  Electron collision energy losses from Ho et. al. 
 # "Chemical reaction Mechanisms for Modeling the Fluorocarbon
 #  Plasma Etch of Silicon Oxide and Related Materials"
 
@@ -39,7 +39,7 @@ set AR_SD_PATH ar_sd.txt
 define_plasma_model name=plasma \
     bulk_model_type=global sheath_model_type=circuit
 
-# CF4 & Ar Species
+# Species from CF4, CHF3, and Ar
 add_species plasma_model=plasma name=CF4   mass=88.003<amu>  charge=0
 add_species plasma_model=plasma name=F2+   mass=37.996<amu>  charge=+1
 add_species plasma_model=plasma name=F2    mass=37.996<amu>  charge=0
@@ -58,8 +58,6 @@ add_species plasma_model=plasma name=F     mass=18.998<amu> charge=0
 add_species plasma_model=plasma name=F-    mass=18.998<amu> charge=-1
 add_species plasma_model=plasma name=Ar    mass=39.948<amu> charge=0
 add_species plasma_model=plasma name=Ar+   mass=39.948<amu> charge=+1
-
-# CHF3 Species
 add_species plasma_model=plasma name=CHF3  mass=70.0128<amu>  charge=0
 add_species plasma_model=plasma name=CHF2+ mass=51.0148<amu>  charge=+1
 add_species plasma_model=plasma name=CHF2  mass=51.0148<amu>  charge=0
@@ -70,147 +68,6 @@ add_species plasma_model=plasma name=H2+   mass=2.0156<amu>   charge=+1
 add_species plasma_model=plasma name=H2    mass=2.0156<amu>   charge=0
 add_species plasma_model=plasma name=H+    mass=1.0078<amu>   charge=+1
 add_species plasma_model=plasma name=H     mass=1.0078<amu>   charge=0
-
-# TABLE 9a - Other CHF3 Reactions
-add_bulk_reaction plasma_model=plasma name=9_6 \
-    expression="e- + CHF3 = CF3+ + H + 2e-" \
-    rate_coefficient_type=arrhenius \
-    a=5.361e-21 b=1.4404 c=15.5106 energy_transfer=15.2<eV>
-
-add_bulk_reaction plasma_model=plasma name=9_7 \
-    expression="e- + CHF3 = CHF2+ + F + 2e-" \
-    rate_coefficient_type=arrhenius \
-    a=2.225e-21 b=1.2886 c=16.424 energy_transfer=16.8<eV>
-
-add_bulk_reaction plasma_model=plasma name=9_8 \
-    expression="e- + CHF3 = CF2+ + HF + 2e-" \
-    rate_coefficient_type=arrhenius \
-    a=6.533e-23 b=1.4404 c=15.5106 energy_transfer=17.6<eV>
-
-add_bulk_reaction plasma_model=plasma name=9_9 \
-    expression="e- + CHF3 = CF+ + 2F = H + 2e-" \
-    rate_coefficient_type=arrhenius \
-    a=6.780e-22 b=1.5225 c=20.1207 energy_transfer=20.9<eV>
-
-add_bulk_reaction plasma_model=plasma name=9_10 \
-    expression="e- + CHF3 = F= + CHF2 + 2e-" \
-    rate_coefficient_type=arrhenius \
-    a=8.120e-20 b=0.9194 c=36.8204 energy_transfer=37.0<eV>
-
-add_bulk_reaction plasma_model=plasma name=9_11 \
-    expression="e- + CHF3 = CHF+ + 2F + 2e-" \
-    rate_coefficient_type=arrhenius \
-    a=1.006e-21 b=1.3223 c=18.2680 energy_transfer=19.8<eV>
-
-add_bulk_reaction plasma_model=plasma name=9_12 \
-    expression="e- + CHF3 = CF3 + H = e-" \
-    rate_coefficient_type=arrhenius \
-    a=3.963e-23 b=1.6416 c=8.9961 energy_transfer=11.0<eV>
-
-add_bulk_reaction plasma_model=plasma name=9_13 \
-    expression="e- + CHF3 = CHF2 + F + e-" \
-    rate_coefficient_type=arrhenius \
-    a=1.187e-22 b=1.3167 c=11.8570 energy_transfer=13.0<eV>
-
-add_bulk_reaction plasma_model=plasma name=9_14 \
-    expression="e- + CHF3 = CF2 + HF + e-" \
-    rate_coefficient_type=arrhenius \
-    a=3.626-20 b=1.0759 c=22.6713 energy_transfer=23.6<eV>
-
-add_bulk_reaction plasma_model=plasma name=9_15 \
-    expression="e- + CHF3 = CF3+ + H + 2e-" \
-    rate_coefficient_type=arrhenius \
-    a=8.084e-19 b=0.5725 c=35.0712 energy_transfer=35.0<eV>
-
-add_bulk_reaction plasma_model=plasma name=9_16 \
-    expression="e- + CHF3 = CF + H + 2F = e-" \
-    rate_coefficient_type=arrhenius \
-    a=6.752e-15 b=0.1877 c=20.3189 energy_transfer=13.3<eV>
-
-add_bulk_reaction plasma_model=plasma name=9_17 \
-    expression="e- + CHF3 = CF3 + H + e-" \
-    rate_coefficient_type=arrhenius \
-    a=2.555e-12 b=-0.4365 c=13.3219 energy_transfer=11.0<eV>
-
-add_bulk_reaction plasma_model=plasma name=9_18_19 \
-    expression="e- + CHF3 = F- + CHF2" \
-    rate_coefficient_type=arrhenius \
-    a=9.1046e-11 b=-1.3618 c=9.7286 energy_transfer=1.3<eV>
-
-add_bulk_reaction plasma_model=plasma name=9_20 \
-    expression="e- + CHF2 = CF2+ + H + 2e-" \
-    rate_coefficient_type=arrhenius \
-    a=5.361e-21 b=1.3438 c=14.9591 energy_transfer=17.2<eV>
-
-add_bulk_reaction plasma_model=plasma name=9_21 \
-    expression="e- + CHF2 = CHF+ + F + 2e-" \
-    rate_coefficient_type=arrhenius \
-    a=2.225e-21 b=1.2886 c=16.4240 energy_transfer=14.3<eV>
-
-add_bulk_reaction plasma_model=plasma name=9_22 \
-    expression="e- + CHF2 = CF+ + HF + 2e-" \
-    rate_coefficient_type=arrhenius \
-    a=6.533e-23 b=1.4404 c=15.5106 energy_transfer=14.6<eV>
-
-add_bulk_reaction plasma_model=plasma name=9_23 \
-    expression="e- + CHF2 = CF2 + H + e-" \
-    rate_coefficient_type=arrhenius \
-    a=3.963e-23 b=1.6416 c=8.9961 energy_transfer=2.71<eV>
-
-add_bulk_reaction plasma_model=plasma name=9_24 \
-    expression="e- + CHF2 = CHF + F + e-" \
-    rate_coefficient_type=arrhenius \
-    a=1.187e-22 b=1.3167 c=11.8570 energy_transfer=4.75<eV>
-
-add_bulk_reaction plasma_model=plasma name=9_25 \
-    expression="e- + CHF2 = CF + H + F + e-" \
-    rate_coefficient_type=arrhenius \
-    a=3.626e-14 b=1.0759 c=22.6713 energy_transfer=8.09<eV>
-
-add_bulk_reaction plasma_model=plasma name=9_29 \
-    expression="e- + H2 = 2H = e-" \
-    rate_coefficient_type=arrhenius \
-    a=1.697e-14 b=-0.0244 c=10.3318 energy_transfer=4.4<eV>
-
-add_bulk_reaction plasma_model=plasma name=9_30 \
-    expression="e- + H2 = H2+ + 2e-" \
-    rate_coefficient_type=arrhenius \
-    a=1.329e-19 b=1.075 c=17.0272 energy_transfer=15.4<eV>
-
-add_bulk_reaction plasma_model=plasma name=9_32 \
-    expression="e- + H = H+ + 2e-" \
-    rate_coefficient_type=arrhenius \
-    a=7.332e-18 b=0.6938 c=14.5972 energy_transfer=13.6<eV>
-
-add_bulk_reaction plasma_model=plasma name=9_33 \
-    expression="F- + CHF2+ = F + CF2 + H" \
-    rate_coefficient_type=arrhenius \
-    a=4e-13 b=-0.5 c=0 energy_transfer=0<eV>
-
-add_bulk_reaction plasma_model=plasma name=9_34 \
-    expression="F- + CHF+ = F + CF + H" \
-    rate_coefficient_type=arrhenius \
-    a=4e-13 b=-0.5 c=0 energy_transfer=0<eV>
-
-add_bulk_reaction plasma_model=plasma name=9_35 \
-    expression="F- + H2+ = F + 2H" \
-    rate_coefficient_type=arrhenius \
-    a=4e-13 b=-0.5 c=0 energy_transfer=0<eV>
-
-add_bulk_reaction plasma_model=plasma name=9_36 \
-    expression="e- + H2+ = 2H" \
-    rate_coefficient_type=arrhenius \
-    a=4e-14 b=0 c=0 energy_transfer=0<eV>
-
-add_bulk_reaction plasma_model=plasma name=9_37 \
-    expression="e- + CHF2+ = H = CF2" \
-    rate_coefficient_type=arrhenius \
-    a=4e-14 b=0 c=0 energy_transfer=0<eV>
-
-add_bulk_reaction plasma_model=plasma name=9_38 \
-    expression="e- + CHF+ = H = CF" \
-    rate_coefficient_type=arrhenius \
-    a=4e-14 b=0 c=0 energy_transfer=0<eV>
 
 # TABLE 1 - CF4 Electron Collisions
 add_bulk_reaction plasma_model=plasma name=1_5 \
@@ -756,6 +613,142 @@ add_bulk_reaction plasma_model=plasma name=ar_ion \
     expression="Ar + e- = Ar+ + 2e-" \
     rate_coefficient_type=arrhenius \
     a=2.3e-14 b=0.6329 c=16.0627 energy_transfer=15.76<eV>
+
+# TABLE 9a - Other CHF3 Reactions
+add_bulk_reaction plasma_model=plasma name=9a_6 \
+    expression="e- + CHF3 = CF3+ + H + 2e-" \
+    rate_coefficient_type=arrhenius \
+    a=5.361e-21 b=1.4404 c=15.5106 energy_transfer=15.2<eV>
+
+add_bulk_reaction plasma_model=plasma name=9a_7 \
+    expression="e- + CHF3 = CHF2+ + F + 2e-" \
+    rate_coefficient_type=arrhenius \
+    a=2.225e-21 b=1.2886 c=16.424 energy_transfer=16.8<eV>
+
+add_bulk_reaction plasma_model=plasma name=9a_8 \
+    expression="e- + CHF3 = CF2+ + HF + 2e-" \
+    rate_coefficient_type=arrhenius \
+    a=6.533e-23 b=1.4404 c=15.5106 energy_transfer=17.6<eV>
+
+add_bulk_reaction plasma_model=plasma name=9a_9 \
+    expression="e- + CHF3 = CF+ + 2F + H + 2e-" \
+    rate_coefficient_type=arrhenius \
+    a=6.780e-22 b=1.5225 c=20.1207 energy_transfer=20.9<eV>
+
+add_bulk_reaction plasma_model=plasma name=9a_10 \
+    expression="e- + CHF3 = F+ + CHF2 + 2e-" \
+    rate_coefficient_type=arrhenius \
+    a=8.120e-20 b=0.9194 c=36.8204 energy_transfer=37.0<eV>
+
+add_bulk_reaction plasma_model=plasma name=9a_11 \
+    expression="e- + CHF3 = CHF+ + 2F + 2e-" \
+    rate_coefficient_type=arrhenius \
+    a=1.006e-21 b=1.3223 c=18.2680 energy_transfer=19.8<eV>
+
+add_bulk_reaction plasma_model=plasma name=9a_12 \
+    expression="e- + CHF3 = CF3 + H + e-" \
+    rate_coefficient_type=arrhenius \
+    a=3.963e-23 b=1.6416 c=8.9961 energy_transfer=11.0<eV>
+
+add_bulk_reaction plasma_model=plasma name=9a_13 \
+    expression="e- + CHF3 = CHF2 + F + e-" \
+    rate_coefficient_type=arrhenius \
+    a=1.187e-22 b=1.3167 c=11.8570 energy_transfer=13.0<eV>
+
+add_bulk_reaction plasma_model=plasma name=9a_14 \
+    expression="e- + CHF3 = CF2 + HF + e-" \
+    rate_coefficient_type=arrhenius \
+    a=3.626e-20 b=1.0759 c=22.6713 energy_transfer=23.6<eV>
+
+add_bulk_reaction plasma_model=plasma name=9a_15 \
+    expression="e- + CHF3 = CHF + 2F + 2e-" \
+    rate_coefficient_type=arrhenius \
+    a=8.084e-19 b=0.5725 c=35.0712 energy_transfer=35.0<eV>
+
+add_bulk_reaction plasma_model=plasma name=9a_16 \
+    expression="e- + CHF3 = CF + H + 2F + e-" \
+    rate_coefficient_type=arrhenius \
+    a=6.752e-15 b=0.1877 c=20.3189 energy_transfer=13.3<eV>
+
+add_bulk_reaction plasma_model=plasma name=9a_18_19 \
+    expression="e- + CHF3 = F- + CHF2" \
+    rate_coefficient_type=arrhenius \
+    a=9.1046e-11 b=-1.3618 c=9.7286 energy_transfer=1.3<eV>
+
+add_bulk_reaction plasma_model=plasma name=9a_20 \
+    expression="e- + CHF2 = CF2+ + H + 2e-" \
+    rate_coefficient_type=arrhenius \
+    a=5.361e-21 b=1.3438 c=14.9591 energy_transfer=17.2<eV>
+
+add_bulk_reaction plasma_model=plasma name=9a_21 \
+    expression="e- + CHF2 = CHF+ + F + 2e-" \
+    rate_coefficient_type=arrhenius \
+    a=2.225e-21 b=1.2886 c=16.4240 energy_transfer=14.3<eV>
+
+add_bulk_reaction plasma_model=plasma name=9a_22 \
+    expression="e- + CHF2 = CF+ + HF + 2e-" \
+    rate_coefficient_type=arrhenius \
+    a=6.533e-23 b=1.4404 c=15.5106 energy_transfer=14.6<eV>
+
+add_bulk_reaction plasma_model=plasma name=9a_23 \
+    expression="e- + CHF2 = CF2 + H + e-" \
+    rate_coefficient_type=arrhenius \
+    a=3.963e-23 b=1.6416 c=8.9961 energy_transfer=2.71<eV>
+
+add_bulk_reaction plasma_model=plasma name=9a_24 \
+    expression="e- + CHF2 = CHF + F + e-" \
+    rate_coefficient_type=arrhenius \
+    a=1.187e-22 b=1.3167 c=11.8570 energy_transfer=4.75<eV>
+
+add_bulk_reaction plasma_model=plasma name=9a_25 \
+    expression="e- + CHF2 = CF + H + F + e-" \
+    rate_coefficient_type=arrhenius \
+    a=3.626e-14 b=1.0759 c=22.6713 energy_transfer=8.09<eV>
+
+add_bulk_reaction plasma_model=plasma name=9a_29 \
+    expression="e- + H2 = 2H + e-" \
+    rate_coefficient_type=arrhenius \
+    a=1.697e-14 b=-0.0244 c=10.3318 energy_transfer=4.4<eV>
+
+add_bulk_reaction plasma_model=plasma name=9a_30 \
+    expression="e- + H2 = H2+ + 2e-" \
+    rate_coefficient_type=arrhenius \
+    a=1.329e-19 b=1.075 c=17.0272 energy_transfer=15.4<eV>
+
+add_bulk_reaction plasma_model=plasma name=9a_32 \
+    expression="e- + H = H+ + 2e-" \
+    rate_coefficient_type=arrhenius \
+    a=7.332e-18 b=0.6938 c=14.5972 energy_transfer=13.6<eV>
+
+add_bulk_reaction plasma_model=plasma name=9a_33 \
+    expression="F- + CHF2+ = F + CF2 + H" \
+    rate_coefficient_type=arrhenius \
+    a=4e-13 b=-0.5 c=0 energy_transfer=0<eV>
+
+add_bulk_reaction plasma_model=plasma name=9a_34 \
+    expression="F- + CHF+ = F + CF + H" \
+    rate_coefficient_type=arrhenius \
+    a=4e-13 b=-0.5 c=0 energy_transfer=0<eV>
+
+add_bulk_reaction plasma_model=plasma name=9a_35 \
+    expression="F- + H2+ = F + 2H" \
+    rate_coefficient_type=arrhenius \
+    a=4e-13 b=-0.5 c=0 energy_transfer=0<eV>
+
+add_bulk_reaction plasma_model=plasma name=9a_36 \
+    expression="e- + H2+ = 2H" \
+    rate_coefficient_type=arrhenius \
+    a=4e-14 b=0 c=0 energy_transfer=0<eV>
+
+add_bulk_reaction plasma_model=plasma name=9a_37 \
+    expression="e- + CHF2+ = H + CF2" \
+    rate_coefficient_type=arrhenius \
+    a=4e-14 b=0 c=0 energy_transfer=0<eV>
+
+add_bulk_reaction plasma_model=plasma name=9a_38 \
+    expression="e- + CHF+ = H + CF" \
+    rate_coefficient_type=arrhenius \
+    a=4e-14 b=0 c=0 energy_transfer=0<eV>
 
 # Solve for species flux distributions in plasma
 define_reactor name=chamber plasma_model=plasma \
